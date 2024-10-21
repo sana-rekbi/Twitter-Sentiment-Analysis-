0619a89504ff7c623253468d3f23c13ef4428c43
sana-rekbi <sanaroukbi@gmail.com>
2024-10-21 08:29:39 +0200

    Suppression du dossier sentiment_emotion_analysis

--- sentiment_emotion_analysis/sentiment_or_emotion/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SentimentOrEmotionConfig(AppConfig):
-    name = 'sentiment_or_emotion'
--- sentiment_emotion_analysis/sentiment_or_emotion/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, redirect, HttpResponse
-from django.views.generic import View, TemplateView
-
-def choose_sentiment_or_emotion(request):
-    return render(request, 'home/home.html')
